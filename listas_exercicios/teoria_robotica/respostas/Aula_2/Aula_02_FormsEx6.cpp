87e79662278edc1c1ac7005b5be73e1e242e8ce5
Pedro Bernardi João <pedro.bernardi.j@gmail.com>
2026-05-13 13:30:25 -0300

    Adicionando exercício 6 da Aula 2 de Teoria da Robótica

---
+++ listas_exercicios/teoria_robotica/respostas/Aula_2/Aula_02_FormsEx6.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    
+    float x {};
+    float y {};
+    float angulo {};
+    
+    float v {};
+    float w {};
+    float dt {};
+
+    cout << "Digite a posição inicial do robô" << endl;
+    cout << "Digite a posição no eixo X (em m): ";
+    cin >> x;
+    cout << "Digite a posição no eixo Y (em m): ";
+    cin >> y;
+    cout << "Digite a angulação (em rad): ";
+    cin >> angulo;
+
+    cout << endl;
+
+    while (true) {
+
+        cout << "Digite o comando de movimento" << endl;
+        cout << "Digite a velocidade linear (em m/s): ";
+        cin >> v;
+        cout << "Digite a velocidade angular (em rad/s): ";
+        cin >> w;
+        cout << "Digite o temṕo (dt) de duração do movimento (em segundos): ";
+        cin >> dt;
+
+        cout << endl;
+
+        if (dt < 0) {
+            
+            cout << "Tempo de movimento (dt) negativo detectado. Encerrando leituras...";
+            break;
+
+        }
+
+        x = x + (v * dt * cos (angulo));
+        y = y + (v * dt * sin (angulo));
+        angulo = angulo + (w * dt);
+
+        cout << "Posicao atualizada:" << endl;
+        cout << "X = " << x << ", Y = " << y << ", Ângulo = " << angulo << endl;
+
+        cout << endl;
+
+    }
+
+    cout << "===== POSIÇÃO FINAL DO ROBÔ =====" << endl;
+    cout << "X = " << x << " m" << endl;
+    cout << "Y = " << y << " m" << endl;
+    cout << "Ângulo = " << angulo << " rad" << endl;
+    cout << "=================================";
+
+    return 0;
+}
---
+++ listas_exercicios/teoria_robotica/respostas/README.md
@@ -0,0 +1,3 @@
+# RESPOSTAS DE TEORIA DA ROBÓTICA
+
+Aqui estão todas as respostas dos exercícios de Teoria da Robótica que foram resolvidos durante o treinamento do Sonho.
